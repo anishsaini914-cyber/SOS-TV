61e80134bd4a1d5fb5b70bad66569a328ceafbe3
anishsaini914-cyber <anishsaini914@gmail.com>
2025-12-17 18:56:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,30 @@
-const CACHE_NAME = "sos-game-v1";
+const CACHE_NAME = "sos-tv-v1";
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/SOS-TV/",
+  "/SOS-TV/index.html",
+  "/SOS-TV/manifest.json",
+  "/SOS-TV/icon-192.png",
+  "/SOS-TV/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+self.addEventListener("install", e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
